e7e6e0e102143656fe567297b1ddf44a122c4c43
Roman Krendyasov <krendyasov_rn@mail.ru>
2024-11-15 15:14:59 +0400

    Initial Commit

---
+++ CompactController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CompactController
+//
+//  Created by Роман Крендясов on 15.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
